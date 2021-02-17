f2eed802c7705a1f732b693b747e7f60aeb0a954
DESKTOP-GBO9HPG\ok <7250603@gmail.com>
2021-02-17 22:08:03 +0300

    final

--- 11homework/js/main.js
+++ 11homework/js/main.js
@@ -21,48 +21,80 @@
 //     };
 // };
 
-
-let pinIterationCounter = 0;
-let pukIterationCounter = 0;
-let pin = '1234';
-let puk = '321';
-let pinFromUser;
-let pukFromUser;
-while(pin != pinFromUser && pukIterationCounter < 4){
-while (pin != pinFromUser && pinIterationCounter != 3){
-    pinFromUser = prompt ('enter PIN code');
-    if (pin != pinFromUser){
-        pinIterationCounter++
-        alert( 3-pinIterationCounter + ' attempts left');
-    }
-    if (pin == pinFromUser){
-        alert('correct pin, wellcome');
-    }
-    if (pin != pinFromUser && pukIterationCounter == 3 && pinIterationCounter == 3){
-        pukIterationCounter++
-        alert ('wrong PIN and PUK code');
+// не по тз !!!
+// let pinIterationCounter = 0;
+// let pukIterationCounter = 0;
+// let pin = '1234';
+// let puk = '321';
+// let pinFromUser;
+// let pukFromUser;
+// while(pin != pinFromUser && pukIterationCounter < 4){
+// while (pin != pinFromUser && pinIterationCounter != 3){
+//     pinFromUser = prompt ('enter PIN code');
+//     if (pin != pinFromUser){
+//         pinIterationCounter++
+//         alert( 3-pinIterationCounter + ' attempts left');
+//     }
+//     if (pin == pinFromUser){
+//         alert('correct pin, wellcome');
+//     }
+//     if (pin != pinFromUser && pukIterationCounter == 3 && pinIterationCounter == 3){
+//         pukIterationCounter++
+//         alert ('wrong PIN and PUK code');
         
-    }
-};
-while (pinIterationCounter == 3 && puk != pukFromUser && pukIterationCounter < 3){
-    pukFromUser = prompt ('enter PUK code');
-    if (puk!=pukFromUser){
-        pukIterationCounter++
-        alert( 3-pukIterationCounter + ' attempts left')
-    }
-    if (puk == pukFromUser){
-        alert('correct puk');
-        pukIterationCounter++
-        pinIterationCounter = 0;
-        pukFromUser = '';
-    }
-    if (pin != pinFromUser && pukIterationCounter == 3 && pinIterationCounter == 3){
-        alert ('wrong PIN and PUK code');
-        pukIterationCounter++
-    }
-};
-};
+//     }
+// };
+// while (pinIterationCounter == 3 && puk != pukFromUser && pukIterationCounter < 3){
+//     pukFromUser = prompt ('enter PUK code');
+//     if (puk!=pukFromUser){
+//         pukIterationCounter++
+//         alert( 3-pukIterationCounter + ' attempts left')
+//     }
+//     if (puk == pukFromUser){
+//         alert('correct puk');
+//         pukIterationCounter++
+//         pinIterationCounter = 0;
+//         pukFromUser = '';
+//     }
+//     if (pin != pinFromUser && pukIterationCounter == 3 && pinIterationCounter == 3){
+//         alert ('wrong PIN and PUK code');
+//         pukIterationCounter++
+//     }
+// };
+// };
 
+//  по тз !!!
+// let pinIterationCounter = 0;
+// let pukIterationCounter = 0;
+// let pin = '1234';
+// let puk = '321';
+// let pinFromUser;
+// let pukFromUser;
+// while(pin != pinFromUser && puk != pukFromUser && pinIterationCounter != 3 && pukIterationCounter != 3){
+// while (pin != pinFromUser && pinIterationCounter != 3){
+//     pinFromUser = prompt ('enter PIN code');
+//     if (pin != pinFromUser){
+//         pinIterationCounter++
+//         alert( 3-pinIterationCounter + ' attempts left');
+//     }
+//     if (pin == pinFromUser){
+//         alert('correct pin, wellcome');
+//     }
+// };
+// while (pinIterationCounter == 3 && puk != pukFromUser && pukIterationCounter < 3){
+//     pukFromUser = prompt ('enter PUK code');
+//     if (puk!=pukFromUser){
+//         pukIterationCounter++
+//         alert( 3-pukIterationCounter + ' attempts left')
+//     }
+//     if (puk == pukFromUser){
+//         alert('correct puk, welcome');
+//     }
+//     if (pin != pinFromUser  && pinIterationCounter == 3 && puk != pukFromUser && pukIterationCounter == 3){
+//         alert ('wrong PIN and PUK code');
+//     }
+// };
+// };
 
 // let iterationCounter = 0;
 // let moreThen100 ;
