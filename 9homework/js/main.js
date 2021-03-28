445441adb83550265b9114969798ee416ba19978
Byrka-Byrka <75577174+Byrka-Byrka@users.noreply.github.com>
2021-03-28 19:45:30 +0300

    add hello world

--- 9homework/js/main.js
+++ 9homework/js/main.js
@@ -2,6 +2,8 @@
 // let userData = prompt('Enter something');
 // alert(userData);
 
+console.log('hello world!');
+
 let firstNumber = Number (prompt('Enter first number'));
 let secondNumber = Number (prompt('Enter second number'));
 let thirdNumber = Number (prompt('Enter third number'));
